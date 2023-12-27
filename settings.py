e9e62b8d5da5653a26b86f752601b4dd2add8aaf
XDFrost <pratham0gupta@gmail.com>
2023-12-27 22:33:06 +0530

    Db updates

--- settings.py
+++ settings.py
@@ -1,7 +1,8 @@
 import pathlib
 
-DISCORD_API_TOKEN = '______________'
+DISCORD_API_TOKEN = 'MTE1NDAxMjg5MjI1ODExNTYwNg.GtCI0_.ocLzceOODWgKJLhiuMvc4uH-bc5EsHEXe1GbFk'
 feedback_channel = 1159938702232010762
+api_key = "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"
 
 base = pathlib.Path(__file__).parent              # gives the path of file's parent file i.e. path of project folder
 cmds = base/"cogs"                          # appending commands folder project path
